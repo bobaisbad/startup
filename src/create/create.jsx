f9ca5468f591ba767e3bdfb7cd0540812006c178
bobaisbad <legomaster1007@gmail.com>
2025-10-08 22:53:12 -0600

    Started implementations of create.jsx and info.jsx

--- src/create/create.jsx
+++ src/create/create.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import './create.css';
 
 export function Create() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>create displayed here</div>
+      <h1>Create An Event</h1>
+            <form method="get" action="calendars.html">
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Event Name: </span>
+                    <input className="form-control" type="text" placeholder="Name" />
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Event Date and Time: </span>
+                    <input className="form-control" type="datetime-local" name="varDatetime" id="datetime" />
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Event Details: </span>
+                    <textarea className="form-control" id="textarea" name="varTextarea"></textarea>
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Event Calendar: </span>
+                    <select className="form-control" id="select" name="varSelect">
+                        <option>Animation</option>
+                        <option selected>CS260</option>
+                        <option>LISRites</option>
+                    </select>
+                </div>
+                <button type="submit" className="btn btn-primary">Create</button>
+            </form>
     </main>
   );
 }
--- src/info/info.jsx
+++ src/info/info.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import './info.css';
 
 export function Info() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>info displayed here</div>
+        <div>
+            <h1>Event Name</h1>
+        </div>
+        <div>
+            <p>
+                Event Date and Time: ??/??/???? ??:?? ?M
+            </p>
+
+            <p>
+                Event Details: blah blah blah
+            </p>
+
+            <p>
+                Event Poster: Bob
+            </p>
+
+            <p>
+                RSVPs: 2
+            </p>
+
+            <form method="get" action="">
+                <div className="input-group mb-3">
+                    <span>
+                        Post Your Thoughts: 
+                    </span>
+                    <textarea className="form-control" id="textarea" name="varTextarea"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Post</button>
+                <button type="submit" className="btn btn-primary">RSVP</button>
+            </form>
+        </div>
+
+        <div className="div-left">
+            <p>
+                Gabriel (1d): Awesome, lets do it!
+            </p>
+
+            <p>
+                Jakob (5h): It sounds fun, but idk if I can come
+            </p>
+        </div>
     </main>
   );
 }
