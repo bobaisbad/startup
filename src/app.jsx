57927a19e4cab52cf4fb7c5df9ba7c424f525c61
bobaisbad <legomaster1007@gmail.com>
2025-10-08 22:36:42 -0600

    Wrapped App in BrowserRouter and updated hyperlinks

--- src/app.jsx
+++ src/app.jsx
@@ -2,58 +2,83 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { About } from './about/about';
+import { Calendars } from './calendars/calendars';
+import { Create } from './create/create';
+import { Info } from './info/info';
+import { Register } from './register/register';
+
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-      {/* <header className="container-fluid">
-        <nav className="navbar fixed-top navbar-dark">
-          <div className="navbar-brand">
-            Simon<sup>&reg;</sup>
-          </div>
-          <menu className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="index.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="play.html">
-                Play
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="scores.html">
-                Scores
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="about.html">
-                About
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header> */}
+    <BrowserRouter>
+      <div className="body bg-dark text-light">
+        {/* <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">
+              Simon<sup>&reg;</sup>
+            </div>
+            <menu className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="play.html">
+                  Play
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="scores.html">
+                  Scores
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="about.html">
+                  About
+                </a>
+              </li>
+            </menu>
+          </nav>
+        </header> */}
 
-        <header className="container-fluid">
-            <nav className="navbar fixed-top navbar-dark">
-                <span className="navbar-brand">Get With Friends</span>
-                <menu className="navbar-nav">
+          <header className="container-fluid">
+              <nav className="navbar fixed-top navbar-dark">
+                  <span className="navbar-brand">Get With Friends</span>
+                  <menu className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="about.html">About</a>
-                    </li> 
-                </menu>
-            </nav>
-        </header>
+                          <NavLink className="nav-link" to="login">Login</NavLink>
+                      </li> 
+                      <li className="nav-item">
+                          <NavLink className="nav-link" to="calendars">Calendars</NavLink>
+                      </li> 
+                      <li className="nav-item">
+                          <NavLink className="nav-link" to="create">Create</NavLink>
+                      </li> 
+                      <li className="nav-item">
+                          <NavLink className="nav-link" to="info">Info</NavLink>
+                      </li> 
+                      <li className="nav-item">
+                          <NavLink className="nav-link" to="register">Register</NavLink>
+                      </li> 
+                      <li className="nav-item">
+                          <NavLink className="nav-link" to="about">About</NavLink>
+                      </li> 
+                  </menu>
+              </nav>
+          </header>
 
-      <main>App components go here</main>
+        <main>App components go here</main>
 
-      <footer className="bg-dark text-white-50">
-        <div className="container-fluid">
-          <span className="text-reset">Alec Winn</span>
-          <a class="text-reset" href="https://github.com/bobaisbad/startup.git">GitHub</a>
-        </div>
-      </footer>
-    </div>
+        <footer className="bg-dark text-white-50">
+          <div className="container-fluid">
+            <span className="text-reset">Alec Winn</span>
+            <a class="text-reset" href="https://github.com/bobaisbad/startup.git">GitHub</a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
