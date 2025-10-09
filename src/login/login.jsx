0264777b4dc718c6e2501267a0ade8950c054f2d
bobaisbad <legomaster1007@gmail.com>
2025-10-08 22:59:05 -0600

    Started implementations of login.jsx and register.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 export function Login() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+      <h1>Welcome to Get With Friends</h1>
+        <form method="get" action="calendars.html">
+            <div className="input-group mb-3">
+                <span className="input-group-text">@</span>
+                <input className="form-control" type="text" placeholder="your@email.com" />
+            </div>
+            <div className="input-group mb-3">
+                <span className="input-group-text">🔒</span>
+                <input className="form-control" type="password" placeholder="password" />
+            </div>
+            <button type="submit" className="btn btn-primary">Login</button>
+            <button type="submit" className="btn btn-secondary" to="register.html">Register</button>
+        </form>
     </main>
   );
 }
--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './register.css';
 
 export function Register() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>register displayed here</div>
+      <h1>Registration</h1>
+        <form method="get" action="calendars.html">
+            <div className="input-group mb-3">
+                <span className="input-group-text">@</span>
+                <input className="form-control" type="text" placeholder="your@email.com" />
+            </div>
+            <div className="input-group mb-3">
+                <span className="input-group-text">🔒</span>
+                <input className="form-control" type="password" placeholder="password" />
+            </div>
+            <div className="input-group mb-3">
+                <span className="input-group-text">🔒</span>
+                <input className="form-control" type="password" placeholder="repeat your password" />
+            </div>
+            <button type="submit" className="btn btn-primary">Create</button>
+        </form>
     </main>
   );
 }
