e8eb7fb66092c8f768c9aae5e53bd2a834c5f977
bobaisbad <legomaster1007@gmail.com>
2025-11-05 10:15:32 -0700

    Implemented calendar updates to allow the correct dates of the week to show at the top

--- src/calendars/calendars.jsx
+++ src/calendars/calendars.jsx
@@ -5,8 +5,68 @@ import { Base } from './base';
 
 export function Calendars() {
     const [events, setEvents] = React.useState([]);
+    const [week, setWeek] = React.useState([]);
 
     // Base(events, setEvents);
+
+    const getNext = (month, day) => {
+        let long = [0, 2, 4, 6, 7, 9, 11]; // 31
+        let med = [3, 5, 8, 10]; // 30
+        let short = [1]; // 28
+        let next = 0;
+
+        if (month in long) {
+            next = day % 32;
+        } else if (month in med) {
+            next = day % 31;
+        } else if (month in short) {
+            next = day % 29;
+        }
+
+        next = next < day ? next + 1 : next;
+
+        return next;
+    };
+
+    const getPrev = (month, day) => {
+        let long = [0, 1, 3, 5, 7, 8, 10]; // 31
+        let med = [4, 5, 6, 9, 11]; // 30
+        let short = [2]; // 28
+        let prev = 0;
+
+        if (month in long) {
+            prev = day === 0 ? 31 : day % 31;
+        } else if (month in med) {
+            prev = day === 0 ? 30 : day % 30;
+        } else if (month in short) {
+            prev = day === 0 ? 28 : day % 28;
+        }
+
+        return prev;
+    };
+
+    const getDate = (month, day) => {
+        let newDay = day;
+
+        if (day > 21) {
+            newDay = getNext(month, day);
+        } else if (day < 7) {
+            newDay = getPrev(month, day);
+        }
+
+        if (newDay < day) {
+            month += 1;
+        } else if (newDay > day) {
+            month -= 1;
+        }
+
+        month = month < 10 ? '0' + month : month;
+        day = day < 10 ? '0' + day : day;
+
+        const date = '' + month + '/' + day + '';
+
+        return date;
+    };
 
     React.useEffect(() => {
         // console.log("Is it here?");
@@ -29,58 +89,126 @@ export function Calendars() {
         const date = today.getDate();
         const month = today.getMonth();
         const year = today.getFullYear();
-        var min = 0;
-        // var minMonth = month
-        var max = 0;
-        var mod = 0;
+        // let week = []
+        // var min = 0;
+        // // var minMonth = month
+        // var max = 0;
+        // let mod = 0;
 
-        if ((month === '0' || month === '2' || month === '4' || month === '6' || month === '7' || month === '9' || month === '11') && date >= 7) {
-            mod = 31;
-        } else if ((month === '3'  || month === '5'  || month === '8'  || month === '10') && date >= 7) {
-            mod = 30;
-        } else if ((month === '0' || month === '1' || month === '3' || month === '5' || month === '7' || month === '8' || month === '10') && date < 7) {
-            mod = 31;
-        } else if ((month === '4'  || month === '6'  || month === '9'  || month === '11') && date < 7) {
-            mod = 30;
-        } else {
-            mod = 28;
-        } 
+        // if ((month === 0 || month === 2 || month === 4 || month === 6 || month === 7 || month === 9 || month === 11) && date >= 7) {
+        //     mod = 31;
+        // } else if ((month === 3 || month === 5 || month === 8 || month === 10) && date >= 7) {
+        //     mod = 30;
+        // } else if ((month === 0 || month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10) && date < 7) {
+        //     mod = 31;
+        // } else if ((month === 4 || month === 6 || month === 9 || month === 11) && date < 7) {
+        //     mod = 30;
+        // } else {
+        //     mod = 28;
+        // }
 
-        if (day === 'Mon') {
-            min = date;
-            max = (date + 6) % mod;
-        } else if (day === 'Tue') {
-            min = (date - 1) % mod;
-            max = (date + 5) % mod;
-        } else if (day === 'Wed') {
-            min = (date - 2) % mod;
-            max = (date + 4) % mod;
-        } else if (day === 'Wed') {
-            min = (date - 3) % mod;
-            max = (date + 3) % mod;
-        } else if (day === 'Wed') {
-            min = (date - 4) % mod;
-            max = (date + 2) % mod;
-        } else if (day === 'Wed') {
-            min = (date - 5) % mod;
-            max = (date + 1) % mod;
-        } else {
-            min = (date - 6) % mod;
-            max = date;
+        switch (day) {
+            case 'Mon':
+                // month = date > (mod - 7) ? 0 : 0
+                week.append(getDate(month, date));
+                week.append(getDate(month, date + 1));
+                week.append(getDate(month, date + 2));
+                week.append(getDate(month, date + 3));
+                week.append(getDate(month, date + 4));
+                week.append(getDate(month, date + 5));
+                week.append(getDate(month, date + 6));
+                break;
+            case 'Tue':
+                week.append(getDate(month, date - 1));
+                week.append(getDate(month, date));
+                week.append(getDate(month, date + 1));
+                week.append(getDate(month, date + 2));
+                week.append(getDate(month, date + 3));
+                week.append(getDate(month, date + 4));
+                week.append(getDate(month, date + 5));
+                break;
+            case 'Wed':
+                week.append(getDate(month, date - 2));
+                week.append(getDate(month, date - 1));
+                week.append(getDate(month, date));
+                week.append(getDate(month, date + 1));
+                week.append(getDate(month, date + 2));
+                week.append(getDate(month, date + 3));
+                week.append(getDate(month, date + 4));
+                break;
+            case 'Thu':
+                week.append(getDate(month, date - 3));
+                week.append(getDate(month, date - 2));
+                week.append(getDate(month, date - 1));
+                week.append(getDate(month, date));
+                week.append(getDate(month, date + 1));
+                week.append(getDate(month, date + 2));
+                week.append(getDate(month, date + 3));
+                break;
+            case 'Fri':
+                week.append(getDate(month, date - 4));
+                week.append(getDate(month, date - 3));
+                week.append(getDate(month, date - 2));
+                week.append(getDate(month, date - 1));
+                week.append(getDate(month, date));
+                week.append(getDate(month, date + 1));
+                week.append(getDate(month, date + 2));
+                break;
+            case 'Sat':
+                week.append(getDate(month, date - 5));
+                week.append(getDate(month, date - 4));
+                week.append(getDate(month, date - 3));
+                week.append(getDate(month, date - 2));
+                week.append(getDate(month, date - 1));
+                week.append(getDate(month, date));
+                week.append(getDate(month, date + 1));
+                break;
+            case 'Sun':
+                week.append(getDate(month, date - 6));
+                week.append(getDate(month, date - 5));
+                week.append(getDate(month, date - 4));
+                week.append(getDate(month, date - 3));
+                week.append(getDate(month, date - 2));
+                week.append(getDate(month, date - 1));
+                week.append(getDate(month, date));
+                break
         }
 
-        if (min <= 0) {
-            min = mod + min
-        }
+        // if (day === 'Mon') {
+        //     min = date;
+        //     max = (date + 6) % mod;
+        // } else if (day === 'Tue') {
+        //     min = (date - 1) % mod;
+        //     max = (date + 5) % mod;
+        // } else if (day === 'Wed') {
+        //     min = (date - 2) % mod;
+        //     max = (date + 4) % mod;
+        // } else if (day === 'Wed') {
+        //     min = (date - 3) % mod;
+        //     max = (date + 3) % mod;
+        // } else if (day === 'Wed') {
+        //     min = (date - 4) % mod;
+        //     max = (date + 2) % mod;
+        // } else if (day === 'Wed') {
+        //     min = (date - 5) % mod;
+        //     max = (date + 1) % mod;
+        // } else {
+        //     min = (date - 6) % mod;
+        //     max = date;
+        // }
 
-        fetch(`https://holidays.abstractapi.com/v1/?api_key=09c2ff7e9fc84272b6261b18ac24f98c&country=US&year=${year}&month=${month}&day=`)
-            .then((response) => response.json())
-            .then((data) => {
-                data.forEach((item) => {
-                    // ???
-                });
-            })
-            .catch();
+        // if (min <= 0) {
+        //     min = mod + min
+        // }
+
+        // fetch(`https://holidays.abstractapi.com/v1/?api_key=09c2ff7e9fc84272b6261b18ac24f98c&country=US&year=${year}&month=${month}&day=`)
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         data.forEach((item) => {
+        //             // ???
+        //         });
+        //     })
+        //     .catch();
     }, []);
 
     const populate = () => {
@@ -353,6 +481,44 @@ export function Calendars() {
         }
     }
 
+    const weekHeads = (
+        <tr>
+            <th>&nbsp;</th>
+            <th>{week[0]}</th>
+            <th>{week[1]}</th>
+            <th>{week[2]}</th>
+            <th>{week[3]}</th>
+            <th>{week[4]}</th>
+            <th>{week[5]}</th>
+            <th>{week[6]}</th>
+        </tr>
+    );
+
+    // if (events.length) {
+    // for (const [i, day] of week.entries()) {
+    //     weekHeads.push(
+    //         // <tr key={i}>
+    //             {/* <NavLink to="../info">Holiday - All Day</NavLink> */}
+    //             {/* <td>{event.time}</td> */}
+    //             {/* <td><NavLink to={'../info/' + event.monday.id}>{event.monday.name}</NavLink></td>
+    //             <td><NavLink to={'../info/' + event.tuesday.id}>{event.tuesday.name}</NavLink></td>
+    //             <td><NavLink to={'../info/' + event.wednesday.id}>{event.wednesday.name}</NavLink></td>
+    //             <td><NavLink to={'../info/' + event.thursday.id}>{event.thursday.name}</NavLink></td>
+    //             <td><NavLink to={'../info/' + event.friday.id}>{event.friday.name}</NavLink></td>
+    //             <td><NavLink to={'../info/' + event.saturday.id}>{event.saturday.name}</NavLink></td>
+    //             <td><NavLink to={'../info/' + event.sunday.id}>{event.sunday.name}</NavLink></td> */}
+    //         <th></th>
+    //         // <td><NavLink to={'../' + event.tuesday.id}>{event.tuesday.name}</NavLink></td>
+    //         // <td><NavLink to={'../' + event.wednesday.id}>{event.wednesday.name}</NavLink></td>
+    //         // <td><NavLink to={'../' + event.thursday.id}>{event.thursday.name}</NavLink></td>
+    //         // <td><NavLink to={'../' + event.friday.id}>{event.friday.name}</NavLink></td>
+    //         // <td><NavLink to={'../' + event.saturday.id}>{event.saturday.name}</NavLink></td>
+    //         // <td><NavLink to={'../' + event.sunday.id}>{event.sunday.name}</NavLink></td>
+    //         // </tr>
+    //     );
+    // }
+    // }
+
     return (
         <main className="container-fluid bg-secondary text-center">
             <hr />
@@ -375,7 +541,8 @@ export function Calendars() {
                         <th>Saturday</th>
                         <th>Sunday</th>
                     </tr>
-                    <tr>
+                    {weekHeads}
+                    {/* <tr>
                         <th>&nbsp;</th>
                         <th>09/22</th>
                         <th>09/23</th>
@@ -384,7 +551,7 @@ export function Calendars() {
                         <th>09/26</th>
                         <th>09/27</th>
                         <th>09/28</th>
-                    </tr>
+                    </tr> */}
                 </thead>
                 <tbody id='events'>
                     {eventRows}
