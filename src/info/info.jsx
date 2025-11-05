4057db9c6514dab8bf331f19f44078a55d0d6138
bobaisbad <legomaster1007@gmail.com>
2025-11-05 13:21:40 -0700

    Wrangling with info to get the details displayed

--- src/info/info.jsx
+++ src/info/info.jsx
@@ -5,6 +5,30 @@ import { useParams, Navigate } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
 
+async function getEvent(id) {
+    let details = [];
+    let event;
+    await fetch('/api/details')
+        // method: 'POST',
+        .then((response) => response.json())
+        .then((given) => {
+            details = given;
+            // console.log(details);
+            console.log(details);
+    });
+
+    for (const [key, value] of Object.entries(details)) {
+        if (key === id) {
+            event = value;
+        }
+    }
+
+    // if (!event) {
+    //     return <Navigate to="../not-found" />;
+    // }
+
+    return event;
+}
 
 export function Info({ name }) {
     // const [comments, setComments] = React.useState([]);
@@ -31,31 +55,121 @@ export function Info({ name }) {
 
     console.log(id)
 
-    if (!/^\d+$/.test(id)) {
-        return <Navigate to="../not-found" />;
-    }
+    // if (!/^\d+$/.test(id)) {
+    //     return <Navigate to="../not-found" />;
+    // }
 
-    let details = [];
-    fetch('/api/details')
-        // method: 'POST',
-        .then((response) => response.json())
-        .then((given) => {
-            details = given;
-    });
+    // let details = [];
+    let event = getEvent(id);
+    // fetch('/api/details')
+    //     // method: 'POST',
+    //     .then((response) => response.json())
+    //     .then((given) => {
+    //         details = given;
+    //         // console.log(details);
+    //         console.log(details);
 
-    let event;
-    for (const [key, value] of details.entries()) {
-        if (key === id) {
-            event = value;
-        }
-    }
+    //         for (const [key, value] of Object.entries(details)) {
+    //             if (key === id) {
+    //                 event = value;
+    //             }
+    //         }
+
+    //         if (!event) {
+    //             return <Navigate to="../not-found" />;
+    //         }
+
+        //     return (
+        //         <main className="container-fluid bg-secondary text-center">
+        //             <div>
+        //                 <h1>{event.name}</h1>
+        //             </div>
+        //             <div>
+        //                 <p>
+        //                     {/* Event Date and Time: ??/??/???? ??:?? ?M */}
+        //                     Event Date and Time: {event.date} {event.time}
+        //                 </p>
+
+        //                 <p>
+        //                     Event Details: {event.details}
+        //                 </p>
+
+        //                 <p>
+        //                     Event Poster: {event.poster}
+        //                 </p>
+
+        //                 {/* <p>
+        //                     RSVPs: {event.rsvps}
+        //                 </p> */}
+
+        //                 {/* <form method="get" action="">
+        //                     <div className="input-group mb-3">
+        //                         <span>
+        //                             Post Your Thoughts:&nbsp;&nbsp;
+        //                         </span>
+        //                         <textarea className="form-control" id="textarea" onChange={(e) => text = e.target.value} name="varTextarea"></textarea>
+        //                     </div> */}
+        //                     {/* <Button variant="primary" onClick={() => {event.comments.push({ name: name, text: text}); console.log(event.comments);}}>Post</Button> */}
+        //                     {/* <Button variant="primary" onClick={() => {}}>Post</Button> */}
+        //                     {/* <Button variant="primary" onClick={() => event.rsvps += 1}>RSVP</Button> */}
+        //                 {/* </form> */}
+        //             </div>
+
+        //             <div className="div-left">
+        //                 {/* <RenderComments event={event} /> */}
+        //                 {/* <div>{comments}</div> */}
+        //                 {/* <p>
+        //                     Gabriel (1d): Awesome, lets do it!
+        //                 </p>
+
+        //                 <p>
+        //                     Jakob (5h): It sounds fun, but idk if I can come
+        //                 </p> */}
+        //             </div>
+
+        //             {/* <Routes> */}
+        //                 {/* <Route path='/0' element={<Info />}></Route> */}
+        //             {/* <Route path='/' element={<Login userName={userName} authState={authState}
+        //                                             onAuthChange={(userName, authState) => {
+        //                                                             setAuthState(authState);
+        //                                                             setUserName(userName);
+        //                                                             }}/>
+        //                                     } exact />
+        //             <Route path='/calendars' element={<Calendars />} />
+        //             <Route path='/create' element={<Create />} />
+        //             <Route path='/info' element={<Info />} />
+        //             <Route path='/register' element={<Register userName={userName} // authState={authState}
+        //                                                         // onAuthChange={(userName, authState) => {
+        //                                                         //               setAuthState(authState);
+        //                                                         //               setUserName(userName);
+        //                                                         //               }}/>
+        //                                                         onRegister={(registerUserName) => {
+        //                                                                     setAuthState(true);
+        //                                                                     setUserName(registerUserName);
+        //                                                                     }}/>
+        //                                             } />
+        //             <Route path='/about' element={<About />} />
+        //             <Route path='*' element={<NotFound />} /> */}
+        //             {/* </Routes> */}
+        //         </main>
+        // );
+    // });
+
+    // console.log("Didn't stop...");
+
+    // let event;
+    // for (const [key, value] of details.entries()) {
+    //     if (key === id) {
+    //         event = value;
+    //     }
+    // }
 
     if (!event) {
         return <Navigate to="../not-found" />;
     }
 
-    console.log(`Returned event:`);
-    console.log(event);
+    // console.log(`Returned event:`);
+    // console.log(event);
 
     // const eventOBJ = localStorage.getItem(id);
 
